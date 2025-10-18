14d0305333d16ab8f67af6157f4b1991977a08ce
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-10-18 17:29:57 +0800

    Adding robust error handling for non-numerics

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn cli() -> Command {
         .subcommand_required(true)
         .subcommand(
             Command::new("codist")
+                .aliases(["co_dist", "comoving_distance", "CoDist"])
                 .about("Calculate comoving distance in Mpc")
                 .arg(
                     Arg::new("input")
@@ -25,6 +26,7 @@ fn cli() -> Command {
         )
         .subcommand(
             Command::new("lumdist")
+                .aliases(["lum_dist", "luminosity_distance", "LumDist"])
                 .about("Calculate the luminosity distance in Mpc")
                 .arg(
                     Arg::new("input")
@@ -42,6 +44,7 @@ fn cli() -> Command {
         )
         .subcommand(
             Command::new("covol")
+                .aliases(["co_vol", "comoving_volume", "CoVol"])
                 .about("Calculate the co-moving volume in Gpc^3.")
                 .arg(
                     Arg::new("input")
@@ -59,6 +62,7 @@ fn cli() -> Command {
         )
         .subcommand(
             Command::new("lookback")
+                .aliases(["look_back", "look_back_time", "lookback_time", "TravelTime"])
                 .about("Calculate the lookback time in Gyr")
                 .arg(
                     Arg::new("input")
@@ -76,6 +80,7 @@ fn cli() -> Command {
         )
         .subcommand(
             Command::new("age")
+                .aliases(["Age", "UniAge"])
                 .about("Calculate the age of the universe in Gyr at a given redshift.")
                 .arg(
                     Arg::new("input")
@@ -93,9 +98,20 @@ fn cli() -> Command {
         )
         .subcommand(
             Command::new("distmod")
+                .aliases(["DistanceMod", "DistMod", "Distmod", "distance_modulus", "dist_mod"])
                 .about("Distance modulus at a given redshift")
                 .arg(Arg::new("z").required(true).index(1).help("redshift")),
         )
+}
+
+fn try_parse_string_to_f64(string: &String) -> f64 {
+    match string.parse::<f64>() {
+        Ok(value) => value,
+        Err(_) => {
+            println!("Value {:?} is not a valid number.", string);
+            std::process::exit(1)
+        }
+    }
 }
 
 fn main() {
@@ -109,8 +125,7 @@ fn main() {
 
     match matches.subcommand() {
         Some(("codist", sub_matches)) => {
-            let value = sub_matches.get_one::<String>("input").unwrap();
-            let value = value.parse::<f64>().expect("Value must be a number.");
+            let value = try_parse_string_to_f64(sub_matches.get_one::<String>("input").unwrap());
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {:?}", cosmo.inverse_codist(value));
             } else {
@@ -119,8 +134,7 @@ fn main() {
         }
 
         Some(("lumdist", sub_matches)) => {
-            let value = sub_matches.get_one::<String>("input").unwrap();
-            let value = value.parse::<f64>().expect("Value must be a number");
+            let value = try_parse_string_to_f64(sub_matches.get_one::<String>("input").unwrap());
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {}", cosmo.inverse_codist(value));
             } else {
@@ -129,8 +143,7 @@ fn main() {
         }
 
         Some(("covol", sub_matches)) => {
-            let value = sub_matches.get_one::<String>("input").unwrap();
-            let value = value.parse::<f64>().expect("Value must be a number");
+            let value = try_parse_string_to_f64(sub_matches.get_one::<String>("input").unwrap());
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {}", cosmo.inverse_covol(value * 1e9));
             } else {
@@ -139,8 +152,7 @@ fn main() {
         }
 
         Some(("lookback", sub_matches)) => {
-            let value = sub_matches.get_one::<String>("input").unwrap();
-            let value = value.parse::<f64>().expect("Value must be a number");
+            let value = try_parse_string_to_f64(sub_matches.get_one::<String>("input").unwrap());
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {}", cosmo.inverse_lookback_time(value));
             } else {
@@ -149,8 +161,7 @@ fn main() {
         }
 
         Some(("age", sub_matches)) => {
-            let value = sub_matches.get_one::<String>("input").unwrap();
-            let value = value.parse::<f64>().expect("Value must be a number");
+            let value = try_parse_string_to_f64(sub_matches.get_one::<String>("input").unwrap());
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {}", cosmo.inverse_age(value));
             } else {
@@ -159,8 +170,7 @@ fn main() {
         }
 
         Some(("distmod", sub_matches)) => {
-            let redshift = sub_matches.get_one::<String>("z").unwrap();
-            let redshift = redshift.parse::<f64>().expect("Value must be a number");
+            let redshift = try_parse_string_to_f64(sub_matches.get_one::<String>("z").unwrap());
             println!("distance modulus = {}", cosmo.distance_modulus(redshift));
         }
 
