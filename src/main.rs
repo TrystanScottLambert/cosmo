6d6bf3095bda8480f1f73264227c1d83f36b3daf
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-10-18 17:05:15 +0800

    Adding distance modulus

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{arg, Command, Arg, ArgAction};
+use clap::{Arg, ArgAction, Command};
 use cosmoxide::Cosmology;
 
 fn cli() -> Command {
@@ -9,88 +9,103 @@ fn cli() -> Command {
         .subcommand(
             Command::new("codist")
                 .about("Calculate comoving distance in Mpc")
-                .arg(Arg::new("input")
-                    .required(true)
-                    .index(1)
-                    .help("Either redshift or comoving distance")
+                .arg(
+                    Arg::new("input")
+                        .required(true)
+                        .index(1)
+                        .help("Either redshift or comoving distance"),
                 )
-                .arg(Arg::new("inverse")
-                    .long("inverse")
-                    .short('i')
-                    .help("Inverse. Redshift at a given comoving distance in Mpc.")
-                    .action(ArgAction::SetTrue)
-                )
+                .arg(
+                    Arg::new("inverse")
+                        .long("inverse")
+                        .short('i')
+                        .help("Inverse. Redshift at a given comoving distance in Mpc.")
+                        .action(ArgAction::SetTrue),
+                ),
         )
-
         .subcommand(
             Command::new("lumdist")
                 .about("Calculate the luminosity distance in Mpc")
-                .arg(Arg::new("input")
-                    .required(true)
-                    .index(1)
-                    .help("Either redshift or luminosity distance in Mpc")
+                .arg(
+                    Arg::new("input")
+                        .required(true)
+                        .index(1)
+                        .help("Either redshift or luminosity distance in Mpc"),
                 )
-                .arg(Arg::new("inverse")
-                    .long("inverse")
-                    .short('i')
-                    .help("Inverse. Redshift at a given comoving distance in Mpc.")
-                    .action(ArgAction::SetTrue)
-                )
+                .arg(
+                    Arg::new("inverse")
+                        .long("inverse")
+                        .short('i')
+                        .help("Inverse. Redshift at a given comoving distance in Mpc.")
+                        .action(ArgAction::SetTrue),
+                ),
         )
-
         .subcommand(
             Command::new("covol")
                 .about("Calculate the co-moving volume in Gpc^3.")
-                .arg(Arg::new("input")
-                    .required(true)
-                    .index(1)
-                    .help("Either redshift or comoving volume in Gpc^3.")
+                .arg(
+                    Arg::new("input")
+                        .required(true)
+                        .index(1)
+                        .help("Either redshift or comoving volume in Gpc^3."),
                 )
-                .arg(Arg::new("inverse")
-                    .long("inverse")
-                    .short('i')
-                    .help("Inverse. Redshift at a given comoving volume in Gpc^3.")
-                    .action(ArgAction::SetTrue)
-                )
+                .arg(
+                    Arg::new("inverse")
+                        .long("inverse")
+                        .short('i')
+                        .help("Inverse. Redshift at a given comoving volume in Gpc^3.")
+                        .action(ArgAction::SetTrue),
+                ),
         )
-
         .subcommand(
             Command::new("lookback")
                 .about("Calculate the lookback time in Gyr")
-                .arg(Arg::new("input")
-                    .required(true)
-                    .index(1)
-                    .help("Either redshift or lookback time in Gyr")
-            )
-                .arg(Arg::new("inverse")
-                    .long("inverse")
-                    .short('i')
-                    .help("Inverse. Redshift at a given lookback time in Gyr")
-                    .action(ArgAction::SetTrue)
+                .arg(
+                    Arg::new("input")
+                        .required(true)
+                        .index(1)
+                        .help("Either redshift or lookback time in Gyr"),
                 )
+                .arg(
+                    Arg::new("inverse")
+                        .long("inverse")
+                        .short('i')
+                        .help("Inverse. Redshift at a given lookback time in Gyr")
+                        .action(ArgAction::SetTrue),
+                ),
         )
-
         .subcommand(
             Command::new("age")
                 .about("Calculate the age of the universe in Gyr at a given redshift.")
-            .arg(Arg::new("input")
-                .required(true)
-                .index(1)
-                .help("Either redshift or age in Gyr")
-            )
-                .arg(Arg::new("inverse")
-                    .long("inverse")
-                    .short('i')
-                    .help("Inverse. Redshift at a given age in Gyr")
-                    .action(ArgAction::SetTrue)
+                .arg(
+                    Arg::new("input")
+                        .required(true)
+                        .index(1)
+                        .help("Either redshift or age in Gyr"),
                 )
+                .arg(
+                    Arg::new("inverse")
+                        .long("inverse")
+                        .short('i')
+                        .help("Inverse. Redshift at a given age in Gyr")
+                        .action(ArgAction::SetTrue),
+                ),
         )
-
+        .subcommand(
+            Command::new("distmod")
+                .about("Distance modulus at a given redshift")
+                .arg(Arg::new("z").required(true).index(1).help("redshift")),
+        )
 }
 
 fn main() {
     let matches = cli().get_matches();
-    let cosmo = Cosmology {omega_m: 0.3, omega_k: 0.0, omega_l: 0.7, h0: 70.};
+    let cosmo = Cosmology {
+        omega_m: 0.3,
+        omega_k: 0.0,
+        omega_l: 0.7,
+        h0: 70.,
+    };
 
     match matches.subcommand() {
         Some(("codist", sub_matches)) => {
@@ -119,7 +134,7 @@ fn main() {
             if *sub_matches.get_one::<bool>("inverse").unwrap_or(&false) {
                 println!("redshift = {}", cosmo.inverse_covol(value * 1e9));
             } else {
-                println!("{} Gpc^3", cosmo.comoving_volume(value)/1e9);
+                println!("{} Gpc^3", cosmo.comoving_volume(value) / 1e9);
             }
         }
 
@@ -141,6 +156,12 @@ fn main() {
             } else {
                 println!("{} Gyr", cosmo.age(value));
             }
+        }
+
+        Some(("distmod", sub_matches)) => {
+            let redshift = sub_matches.get_one::<String>("z").unwrap();
+            let redshift = redshift.parse::<f64>().expect("Value must be a number");
+            println!("distance modulus = {}", cosmo.distance_modulus(redshift));
         }
 
         _ => println!("Command not recognized"),
